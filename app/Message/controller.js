c019332af69aa29dd0504ce00ecefafb9ec43ad1
acepnurmansidik <acepnurmansidik@gmail.com>
2022-01-23 14:04:43 +0700

    Get Message | unsuccess

--- app/Message/controller.js
+++ app/Message/controller.js
@@ -1,4 +1,5 @@
 const jwt_decode = require("jwt-decode");
+const { Op } = require("sequelize");
 const { user, message } = require("../../models");
 
 module.exports = {
@@ -18,6 +19,37 @@ module.exports = {
         where: {
           idSender: dataDecode.id,
           idReceiver: id,
+        },
+        attributes: {
+          exclude: ["createdAt", "updatedAt", "idSender", "idReceiver"],
+        },
+        include: {
+          model: user,
+          as: "user",
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "password", "bio", "email"],
+          },
+        },
+      });
+      res.status(201).json({
+        status: "success",
+        message: "Message send",
+        data: { Message: data },
+      });
+    } catch (err) {
+      res.status(500).json({ status: "failed", message: "Server error" });
+    }
+  },
+  getMessage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const dataDecode = jwt_decode(req.token);
+
+      const data = await message.findAll({
+        idSender: {
+          [Op.or]: {
+            [Op.eq]: [dataDecode.id, id],
+          },
         },
         attributes: {
           exclude: ["createdAt", "updatedAt", "idSender", "idReceiver"],
--- app/Message/router.js
+++ app/Message/router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { isLoginAuthorization } = require("../middleware/index");
-const { actionSendMessage } = require("./controller");
+const { actionSendMessage, getMessage } = require("./controller");
 const router = express.Router();
 
 router.use(isLoginAuthorization);
 router.post("/message/:id", actionSendMessage);
+router.get("/message-user/:id", getMessage);
 
 module.exports = router;
